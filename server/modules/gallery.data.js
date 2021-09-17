48ad4371f4c9598964c03f561811ce99db5ea587
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-16 19:12:18 -0500

    added photos to project and added descriptions to galleryItems array git commi git commit -m

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/cdmx.jpg', description: 'At Teotihuacan outside of Mexico City.', likes: 0},
+    { id: 3, path: 'images/ethiopia.jpg', description: 'Monkey in a tree in western Ethiopia.', likes: 0},
+    { id: 4, path: 'images/frankie.jpg', description: 'My new niece, Frankie!', likes: 0},
+    { id: 5, path: 'images/icecream.jpg', description: 'My sister and I checking out MN Nice Cream in the Sculpture Garden', likes: 0},
+    { id: 6, path: 'images/rhinos.jpg', description: 'Momma and baby rhinos in South Africa.', likes: 0},
+    { id: 7, path: 'images/wildgame.jpg', description: 'Eric and I at a Wild game.', likes: 0},
 ];
 
 module.exports = galleryItems;
